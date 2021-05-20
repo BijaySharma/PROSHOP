4fd4f9fa18abe3ac84ab4a1ae58188deb9bb5470
Bijay Sharma <yoursbijay@outlook.com>
2021-05-20 15:46:03 +0530

    Basic Forntend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+// libraries
+import { Container } from 'react-bootstrap';
+// components
+import Header from './components/Header';
+import Footer from './components/Footer';
+// screens
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Proshop</h1>
+    <Header />
+    <main className="py-3">
+      <Container>
+        <HomeScreen />
+      </Container>
+    </main>
+    <Footer />
     </>
   );
 }
